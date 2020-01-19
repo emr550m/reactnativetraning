2c0e01699e850fc42cd7ac212e488cf338a6d190
Emrah Öz <emr550maranello@hotmail.com>
2020-01-19 22:09:07 +0300

    exercise 11 updates

---
+++ Exercise11/src/components/title.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import {
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+
+
+
+export function Title(props) {
+    return (<Text>Hello World!</Text>);
+}
