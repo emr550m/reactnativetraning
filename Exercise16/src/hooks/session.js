5c107b4de2a1be7ec8ada80504f20a07086d47b8
Emrah Öz <emr550maranello@hotmail.com>
2020-01-26 13:56:49 +0300

    updated

--- Exercise16/src/hooks/session.js
+++ Exercise16/src/hooks/session.js
@@ -2,17 +2,20 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useSelector, useDispatch } from 'react-redux';
 
-export function useLoginCheck() {
+export function useLoginCheck() { 
     const navigation = useNavigation();
     const app = useSelector(state => state.app);
 
     useEffect(() => {
-        if (!app.isLoggedIn)
+        if (!app.isLoggedIn){ 
             navigation.navigate("login");
-    });
+        } 
+    },[app.isLoggedIn]);
 
-    if (!app.isLoggedIn)
+    if (!app.isLoggedIn){ 
         navigation.navigate("login");
+    }
+       
 
     return false;
 }
