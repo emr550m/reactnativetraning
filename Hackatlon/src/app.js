5283d5eeb08efad343f82872f7badd3a856a2ba3
Ozan Uysal <ozan.uysal2@akbank.com>
2020-01-31 15:54:58 +0300

    son olsun

--- Hackatlon/src/app.js
+++ Hackatlon/src/app.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import {
-    SafeAreaView,
-    StyleSheet,
-    View,
-    ImageBackground,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, View, ImageBackground} from 'react-native';
 
-import { Login } from "./pages/login"
+import {Login} from './pages/login';
 
 export function App() {
-    return (<ImageBackground source={require("../asset/run.jpg")} style={{ width: '100%', height: '100%' }}>
-        <SafeAreaView style={styles.safeArea}>
+  return (
+    <ImageBackground
+      source={require('../asset/run.jpg')}
+      style={{width: '100%', height: '100%'}}>
+      <SafeAreaView style={styles.safeArea}>
         <View style={styles.root}>
-            <Login />
+          <Login />
         </View>
-    </SafeAreaView>
-    </ImageBackground >);
-};
+      </SafeAreaView>
+    </ImageBackground>
+  );
+}
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1
-    },
-    root: {
-        flex: 1,
-    }
+  safeArea: {
+    flex: 1,
+  },
+  root: {
+    flex: 1,
+  },
 });
